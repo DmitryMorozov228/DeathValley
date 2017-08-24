18996da19c6659ece033a7ab53983f592bc4e22c
Dmitry <dmitriy_morozov_1997@mail.ru>
2017-08-24 21:14:49 +0300

    Changed methods name in ValuesController

--- DeathValley/Controllers/ValuesController.cs
+++ DeathValley/Controllers/ValuesController.cs
@@ -10,29 +10,31 @@ namespace DeathValley.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<string> GetAllItems()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public string GetItem(int id)
         {
             return "value";
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        [HttpPost]
+        public void CreateItem([FromBody]string value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public void EditItem(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public void DeleteItem(int id)
         {
         }
     }
