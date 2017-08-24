560062fbfb74f2d7bad6a9d65c88652299e61198
Dmitry <dmitriy_morozov_1997@mail.ru>
2017-08-24 22:10:59 +0300

    Add new models Param and CacheData

---
+++ DeathValley/Models/CacheData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeathValley.Models
+{
+    public class CacheData
+    {
+        public int CacheDataId { get; set; }
+        public double PointX { get; set; }
+        public double PointY { get; set; }
+        public int? ParamId { get; set; }
+        public virtual Param Param { get; set; }
+    }
+}
---
+++ DeathValley/Models/Param.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeathValley.Models
+{
+    public class Param
+    {
+        public int ParamId { get; set; }
+        public double CoefficientA { get; set; }
+        public double CoefficientB { get; set; }
+        public double CoefficientC { get; set; }
+        public double Step { get; set; }
+        public double RangeFrom { get; set; }
+        public double RangeTo { get; set; }
+        public virtual ICollection<CacheData> CacheDatas { get; set; }
+    }
+}
