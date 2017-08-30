0cff96d4659d6570775307a5f4a87d7f47b232cc
Dmitry <dmitriy_morozov_1997@mail.ru>
2017-08-30 12:06:42 +0300

    Add app.js, that is contain logic for drawing chart

---
+++ DeathValley/Scripts/app.js
@@ -0,0 +1,42 @@
+﻿var dataForChart;
+
+$('#resForm').submit(function (event) {
+    event.preventDefault();
+    var $form = $(this),
+        url = $form.attr('action');
+
+    var jqxhr = $.post(url, $('#resForm').serialize(),
+        function (jsonData) {
+            console.log(jsonData);
+            dataForChart = jsonData;
+            drawChart();
+        });
+});
+
+google.charts.load('current', { 'packages': ['corechart', 'line'] });
+
+function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Y');
+
+    for (var i = 0; i < dataForChart.length; i++) {
+        data.addRow([dataForChart[i].PointX, dataForChart[i].PointY]);
+    }
+
+    var options = {
+        title: 'The Porabolic function',
+        curveType: 'function',
+        legend: { position: 'right' },
+        hAxis: {
+            title: 'X'
+        },
+        vAxis: {
+            title: 'Y'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('image'));
+
+    chart.draw(data, options);
+}
